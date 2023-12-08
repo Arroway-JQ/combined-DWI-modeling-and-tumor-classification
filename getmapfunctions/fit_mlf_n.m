07659e2d58b90e821d5657cd1c21276cde5f82e2
Arroway-JQ <55519716+Arroway-JQ@users.noreply.github.com>
2023-12-08 21:30:34 +0800

    Update fit_mlf_n.m
    needs rights

--- getmapfunctions/fit_mlf_n.m
+++ getmapfunctions/fit_mlf_n.m
@@ -1,5 +1,5 @@
 function [mlf_coef,ChiSq,residuals,converge] = fit_mlf_n(xdata,ydata,D,A,B,weights)
-% ChiSq 为 拟合度指标
+% ChiSq 脦陋 脛芒潞脧露脠脰赂卤锚
 if isempty(ydata)
     mlf_coef = [0 0 0];
     ChiSq = 0;
@@ -22,18 +22,7 @@ else
     x0 = [1 D alpha_ini beta_ini];
     lb = [1*0.1   0   0    0    ];
     ub = [1*10   1.0  1.5   3.0   ];
-    if isempty(weights)
-        [mlf_coef,resnorm,residuals,converge] = ...
-            fitmod('mlf_model',x0,xdata,ydata,lb,ub);
-    else
-        [mlf_coef,resnorm,residuals,converge] = ...
-            fitmod_weighted('mlf_model',x0,xdata,ydata,weights,lb,ub);
-    end
-    converge = double(converge);
     
-    % 4) Calculate error (mean absolute percent error)
-    sum_y = sum(ydata.^2);
-    ChiSq = 1-(resnorm/sum_y)^(1/2);
     
 end
 
